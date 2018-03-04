5b281302d078bd2fabe0d8e969b425e30c62c0cd
Henri Menke <henri@icp.uni-stuttgart.de>
2018-03-04 17:12:21 +1300

    [tests] Test for generic distance bond

--- testsuite/generic.py
+++ testsuite/generic.py
@@ -17,42 +17,49 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import print_function
 import unittest as ut
+
 import espressomd
+features = ["EXPRESSION", "LENNARD_JONES", "BOND_ANGLE"]
+if espressomd.has_features(features):
+    from espressomd.interactions import GenericBond, HarmonicBond, AngleHarmonic
+
 import numpy as np
 
 # TODO: Feature guard
-@ut.skipIf(not espressomd.has_features("LENNARD_JONES","EXPRESSION"),"Missing features")
+@ut.skipIf(not espressomd.has_features(features),"Missing features")
 class GenericTest(ut.TestCase):
+
+    system = espressomd.System(box_l=[10,10,10])
+    system.time_step = 0.01
+    system.cell_system.skin = 0.1
+
+    pos0 = system.box_l/2 - [.1,0,0]
+    pos1 = system.box_l/2
+    pos2 = system.box_l/2 + [.1,0,0]
+
+    system.part.add(pos=pos0,type=0)
+    system.part.add(pos=pos1,type=0)
+    system.part.add(pos=pos2,type=0)
+
     def setUp(self):
-        self.system = espressomd.System(box_l=[10,10,10])
-        self.system.time_step = 0.01
-        self.system.cell_system.skin = 0.1
-
-        self.pos0 = self.system.box_l/2 - [.1,0,0]
-        self.pos1 = self.system.box_l/2 + [.1,0,0]
-        self.params = {
-            'epsilon': 0.1,
-            'sigma': 0.1,
-            'cutoff': 1.0,
-            'shift': 0.0
-        }
-
-        self.system.part.add(pos=self.pos0,type=0)
-        self.system.part.add(pos=self.pos1,type=0)
+        self.system.part[0].pos = self.pos0
+        self.system.part[1].pos = self.pos1
+        self.system.part[2].pos = self.pos2
+        self.system.part[:].v = [0,0,0]
+        self.system.part[:].f = [0,0,0]
 
     def test_non_bonded(self):
+        params = { 'epsilon': 0.1, 'sigma': 0.1, 'cutoff': 1.0, 'shift': 0.0  }
+
         # Run with LJ
-        self.system.non_bonded_inter[0,0].lennard_jones.set_params(**self.params)
+        self.system.non_bonded_inter[0,0].lennard_jones.set_params(**params)
 
         self.system.integrator.run(10)
 
         reference = np.copy(self.system.part[:].pos)
 
         # Reset particles
-        self.system.part[0].pos = self.pos0
-        self.system.part[1].pos = self.pos1
-        self.system.part[:].v = [0,0,0]
-        self.system.part[:].f = [0,0,0]
+        self.setUp()
 
         # Run with LJ from expression
         self.system.non_bonded_inter[0,0].lennard_jones.set_params(
@@ -60,8 +67,35 @@ class GenericTest(ut.TestCase):
     
         self.system.non_bonded_inter[0,0].generic.set_params(
             cutoff=1.0,
-            energy="4*{epsilon}*(({sigma}/x)**12 - ({sigma}/x)**6)".format(**self.params),
-            force="-48*{epsilon}*(({sigma}/x)**12 - .5*({sigma}/x)**6)/x".format(**self.params))
+            energy="4*{epsilon}*(({sigma}/x)**12 - ({sigma}/x)**6)".format(**params),
+            force="-48*{epsilon}*(({sigma}/x)**12 - .5*({sigma}/x)**6)/x".format(**params))
+
+        self.system.integrator.run(10)
+
+        np.testing.assert_allclose(np.copy(self.system.part[:].pos), reference)
+
+    def test_bonded_distance(self):
+        params = { 'k': 1.0, 'r_0': 1.0, 'r_cut': 1.0 }
+
+        # Run with harmonic bond
+        bond = HarmonicBond(**params)
+        self.system.bonded_inter.add(bond)
+        self.system.part[0].add_bond((bond,1))
+
+        self.system.integrator.run(10)
+
+        reference = np.copy(self.system.part[:].pos)
+
+        # Reset particles
+        self.setUp()
+        self.system.part[0].delete_bond(self.system.part[0].bonds[0])
+
+        # Run with LJ from expression
+        bond = GenericBond(type='distance', cutoff=params['r_cut'],
+                           energy="{k}/2*(x-{r_0})**2".format(**params),
+                           force="{k}*(x-{r_0})".format(**params))
+        self.system.bonded_inter.add(bond)
+        self.system.part[0].add_bond((bond,1))
 
         self.system.integrator.run(10)
 
